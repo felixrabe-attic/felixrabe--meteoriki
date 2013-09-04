d86c643e008b941c7424d6cd0895937077e0c9c3
Felix Rabe <public@felixrabe.net>
2013-09-04 09:26:22 +0200

    wip

---
+++ packages/micro-wiki-mark/package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: "A minimal wiki markup language"
+});
+
+Package.on_use(function (api, where) {
+  // api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  // api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+});
